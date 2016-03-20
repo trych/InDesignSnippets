fc0c1c6bd2b4a8d9d602b33950c95c6c6d03bf7a
trych <timorychert@23x5.net>
2016-03-20 22:53:34 +0100

    added description to RemoveRecentDocs.js

--- Misc/RemoveRecentDocs.js
+++ Misc/RemoveRecentDocs.js
@@ -1,7 +1,8 @@
 #target InDesign
 
+// InDesign script to remove all items from the "Open Recent" list
+
 var userOR = app.generalPreferences.openRecentLength;
 
 app.generalPreferences.openRecentLength = 0;
-
 app.generalPreferences.openRecentLength = userOR;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # InDesignSnippets
 Collection of some InDesign scripting snippets.
+
+Also, finally learning GitHub, so bear with me, while this repository hopefully slowly fills up here.
