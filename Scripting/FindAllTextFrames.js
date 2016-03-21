d9694ecb1c7117141158fae7731179a903ea65b0
trych <timorychert@23x5.net>
2016-03-21 15:15:49 +0100

    Added link to FindAllTextFrames.js

--- Scripting/FindAllTextFrames.js
+++ Scripting/FindAllTextFrames.js
@@ -1,7 +1,8 @@
 #target InDesign
 
-// finds all textFrames in the active document.
+// finds all textFrames in the active document
 // doing it like this is necessary, because app.activeDocument.textFrames does *not* find textFrames within groups or within other textFrames etc.
+// see: http://stackoverflow.com/a/17695293/3399765
 
 var allItems = app.activeDocument.allPageItems;
 var tfCounter = 0;
