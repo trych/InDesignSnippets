ba0226ff964f671e8c047a772379a885758b272e
trych <timorychert@23x5.net>
2016-04-04 07:41:58 +0200

    syntax cleanup and attribution

--- Text/insertionPointXPos.js
+++ Text/insertionPointXPos.js
@@ -1,11 +1,11 @@
 #target indesign
 
 // finds and alerts the x-position of the current insertion point
+// with elements from http://graphicdesign.stackexchange.com/a/48426/21771
 
-if (app.selection[0].constructor.name == "InsertionPoint") {
-  $.writeln("got it IP");
+if (app.selection[0].constructor.name === "InsertionPoint") {
   var ip = app.selection[0];
-  var story = app.selection[0].parentStory;
+  var story = ip.parentStory;
 
   var charBehind = story.characters[(ip.index)];
   var pos = charBehind.horizontalOffset;
